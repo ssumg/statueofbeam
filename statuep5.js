c0671fb5082def05440dbda83752a79290f8672e
ssumg <37644244+ssumg@users.noreply.github.com>
2018-05-10 02:15:25 +0900

    Add files via upload

---
+++ statuep5.js
@@ -0,0 +1,44 @@
+var statue;
+var fire1, fire2;
+var time = 0;
+function setup() {
+  createCanvas(690, 900);
+  background(40, 40, 90);
+  statue = loadImage("assets/statue1.png");
+  fire1 = loadImage("assets/fire1.png");
+  fire2 = loadImage("assets/fire2.png");
+}
+
+function draw() {
+  time += 1;
+  background(40, 40, 90);
+  image(statue, 0, 10);
+  if(time % 124 == 0)
+    background(200, 200, 0);
+}
+function mouseClicked(){
+  noStroke();
+  fill(255, 0, 0);
+  ellipse(345,250, 15, 15);
+  ellipse(365, 250, 15, 15);
+}
+function mouseDragged(){
+  if(time % 4 == 0 || time%3 ==0)
+    image(fire2, 0, 0);
+  else
+    image(fire1, 0, 0);
+  strokeWeight(3);  
+  stroke(255, 0, 0);
+  line(345, 250, mouseX-50, 855);
+  line(365, 250, mouseX+50, 855);
+  
+  strokeWeight(2);
+  stroke(255, 255, 0);
+  line(345, 250, mouseX-50, 855);
+  line(365, 250, mouseX+50, 855);
+  
+  noStroke();
+  fill(120);
+  for(int i = 0; i < 5; i++)
+    ellipse(random(mouseX-90, mouseX+60), 850, random(50), random(50));
+  }
